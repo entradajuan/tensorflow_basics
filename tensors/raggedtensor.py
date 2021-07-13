1a7a145822841b2bce86ccd99893edd0844d4920
entradajuan <entradajuan@yahoo.es>
2021-07-13 18:41:26 +0000

    Tensorflow basics

--- tensors/raggedtensor.py
+++ tensors/raggedtensor.py
@@ -1,3 +1,4 @@
+import math
 import tensorflow as tf
 
 digits = tf.ragged.constant([[3, 1, 4, 1], [], [5, 9, 2], [6], []])
@@ -26,3 +27,40 @@ print(dataset)
 print(type(dataset))
 print(dataset[0])
 print(type(dataset[0]))
+
+data = tf.RaggedTensor.from_value_rowids(
+    values=[3, 1, 4, 1, 5, 9, 2],
+    value_rowids=[0, 0, 0, 0, 2, 2, 3])
+
+print(data)
+
+queries = tf.ragged.constant([['Who', 'is', 'Dan', 'Smith'],
+                              ['Pause'],
+                              ['Will', 'it', 'rain', 'later', 'today']])
+
+
+# Create an embedding table.
+num_buckets = 1024
+embedding_size = 4
+embedding_table = tf.Variable(
+    tf.random.truncated_normal([num_buckets, embedding_size],
+                       stddev=1.0 / math.sqrt(embedding_size)))
+
+# Look up the embedding for each word.
+word_buckets = tf.strings.to_hash_bucket_fast(queries, num_buckets)
+word_embeddings = tf.nn.embedding_lookup(embedding_table, word_buckets)     
+
+# Add markers to the beginning and end of each sentence.
+marker = tf.fill([queries.nrows(), 1], '#')
+padded = tf.concat([marker, queries, marker], axis=1)                       
+
+# Build word bigrams and look up embeddings.
+bigrams = tf.strings.join([padded[:, :-1], padded[:, 1:]], separator='+')   
+
+bigram_buckets = tf.strings.to_hash_bucket_fast(bigrams, num_buckets)
+bigram_embeddings = tf.nn.embedding_lookup(embedding_table, bigram_buckets) 
+
+# Find the average embedding for each sentence
+all_embeddings = tf.concat([word_embeddings, bigram_embeddings], axis=1)    
+avg_embedding = tf.reduce_mean(all_embeddings, axis=1)                      
+print(avg_embedding)
