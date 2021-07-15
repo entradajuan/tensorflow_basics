cada162943c4d627076cd2f296b32f0680082c08
entradajuan <entradajuan@yahoo.es>
2021-07-15 11:37:48 +0000

    Tensorflow basics

--- keras/functional1.py
+++ keras/functional1.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 
 tf.keras.backend.clear_session()
 
-
 inputs = tf.keras.Input(shape=(784,))
 
 img_inputs = tf.keras.Input(shape=(32, 32, 3))
@@ -12,12 +11,15 @@ print(inputs.dtype)
 
 dense = tf.keras.layers.Dense(64, activation='relu')
 x = dense(inputs)
-
 print(x)
 
-x = tf.keras.layers.Dense(64, activation='relu')(x)
-outputs = tf.keras.layers.Dense(10, activation='softmax')(x)
+x2 = tf.keras.layers.Dense(128, activation='relu')(x)
+print(x2)
 
+x3 = tf.keras.layers.Dense(32, activation='relu')(x)
+print(x3)
+
+outputs = tf.keras.layers.Dense(10, activation='softmax')(x3)
 print(outputs)
 
 model = keras.Model(inputs=inputs, outputs=outputs)
