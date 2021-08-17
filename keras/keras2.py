8656c6ef16ba714f6cb654dcc958f43dd60d6297
entradajuan <entradajuan@yahoo.es>
2021-08-17 18:21:30 +0000

    keras fundamentals

--- keras/keras2.py
+++ keras/keras2.py
@@ -1,69 +1,85 @@
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import datasets, layers, models, preprocessing
-import tensorflow_datasets as tfds
-
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 
-## PREPROCESS _________________
-
+# 1 .- LOAD DATA
 data = pd.read_csv('importing/spam.csv', encoding = "ISO-8859-1")
-data = data[['v1','v2']] 
+data = data[['v1', 'v2']]
 
-conver = lambda s : int(s=='ham') 
+# 2 .- PREPROCESS DATA
+convert = lambda s : int(s=='ham')
+data['v1'] = data['v1'].apply(convert)
 
-data['v1'] = data['v1'].apply(conver)
-#print(data[['v1','v2']].head())
-#print(type(data))
-#print(data.describe())
+training_data = data.sample(frac=0.8, random_state=25)
+testing_data = data.drop(training_data.index)
 
-label = data['v1']
-sentences = data['v2']
-#print(sentences)
+sentences = training_data['v2']
+labels = training_data['v1']
+
+print(sentences[3])
+
+tokenizer = tf.keras.preprocessing.text.Tokenizer()
+tokenizer.fit_on_texts(data['v2'])
+sentences = tokenizer.texts_to_sequences(sentences)
+
+print(sentences[3])
 print(type(sentences))
 
-## VECTORIZE
-tokenizer = tf.keras.preprocessing.text.Tokenizer()
-tokenizer.fit_on_texts(sentences)
-text_sequences = tokenizer.texts_to_sequences(sentences)
+MAX_LEN = 200
+sentences = tf.keras.preprocessing.sequence.pad_sequences(sentences, maxlen=MAX_LEN)
 
-text_sequences = tf.keras.preprocessing.sequence.pad_sequences(text_sequences)
-print(len(text_sequences[0]))
-print(len(text_sequences[5571]))
+print(sentences[3])
+print(type(sentences))
 
-## MODEL
-N_HIDDEN = 25
+# 3 .- BUILD MODEL
+N_HIDDEN = 250
 OUTPUT_BITS = 1
 DROPOUT = 0.2
-EPOCHS = 1
+EPOCHS = 50
 BATCH_SIZE = 50
+INPUT_SHAPE = (len(sentences[3]), )
+print(INPUT_SHAPE)
 
-#build the model
 model = tf.keras.models.Sequential()
-model.add(keras.layers.Dense(N_HIDDEN,
-   		input_shape=(len(text_sequences[0]),),
-   		name='dense_layer', activation='relu'))
-model.add(keras.layers.Dropout(DROPOUT))
-model.add(keras.layers.Dense(N_HIDDEN,
+model.add(tf.keras.layers.Dense(N_HIDDEN,
+   		input_shape=INPUT_SHAPE,
+   		name='dense_layer_1', activation='relu'))
+model.add(tf.keras.layers.Dropout(DROPOUT))
+model.add(tf.keras.layers.Dense(N_HIDDEN,
    		name='dense_layer_2', activation='relu'))
-model.add(keras.layers.Dropout(DROPOUT))
-model.add(keras.layers.Dense(OUTPUT_BITS,
+model.add(tf.keras.layers.Dropout(DROPOUT))
+model.add(tf.keras.layers.Dense(OUTPUT_BITS,
    		name='dense_layer_3', activation='softmax'))
 
-
-
-#model = tf.keras.models.Sequential()
-#model.add(layers.Dense(128, input_shape=(189,), activation='relu'))
-#model.add(layers.Dropout(0.5))
-#model.add(layers.Dense(1, activation='sigmoid'))
-
-# summary of the model
 model.summary()
-
 model.compile(optimizer = "adam", loss = "binary_crossentropy",metrics = ["accuracy"])
 
-#label = np.asarray(label).astype('float32').reshape((-1,1))
-#X_test, y_test = 
-#score = model.fit(text_sequences, label, epochs= EPOCHS, batch_size = BATCH_SIZE, validation_data = (X_test, y_test))
-score = model.fit(text_sequences, label, epochs= EPOCHS, batch_size = BATCH_SIZE)
+# 4 .- BUILD MODEL
+score = model.fit(sentences, labels, epochs= EPOCHS, batch_size = BATCH_SIZE)
+
+# 5 .- EVALUATE MODEL
+eval_sentences = testing_data['v2']
+eval_labels = testing_data['v1']
+
+eval_sentences = tokenizer.texts_to_sequences(eval_sentences)
+eval_sentences = tf.keras.preprocessing.sequence.pad_sequences(eval_sentences, maxlen=MAX_LEN)
+
+score = model.evaluate(eval_sentences, eval_labels, batch_size=BATCH_SIZE)
+print("\nTest score:", score[0])
+print('Test accuracy:', score[1])
+
+# 6 .- USE MODEL TO PREDICT
+#text = ['Hi Juani, I will need an answer tomorrow in the morning. regards']
+text = ['Hi, Get the new iPhone in this link and get a 50% saving!!. Best regards']
+
+sentence = tokenizer.texts_to_sequences(text)
+print(sentence)
+sentence = tf.keras.preprocessing.sequence.pad_sequences(sentence, maxlen=MAX_LEN)
+print(sentence)
+
+print(type(sentence))
+prediction = model.predict(sentence)
+print(prediction)
+
+
+
