078ce26f9c0a10d05e2970d386ac9328953fe4d5
entradajuan <entradajuan@yahoo.es>
2021-08-18 17:31:05 +0000

    keras fundamentals 3

--- keras/keras3/utils3.py
+++ keras/keras3/utils3.py
@@ -1,8 +1,8 @@
+import tensorflow as tf
 
-def get_tokenizer2(data):
+def get_tokenizer(data):
   tokenizer = tf.keras.preprocessing.text.Tokenizer()
   tokenizer.fit_on_texts(data)
 
   return tokenizer
 
-  
