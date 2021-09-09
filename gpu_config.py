ce3cee6c3982d660adc9f5000071b6a64afd8901
entradajuan <entradajuan@yahoo.es>
2021-09-09 11:44:48 +0000

    gpu

---
+++ gpu_config.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+
+print(tf.version)
+print(tf.__version__)
+
+tf.keras.backend.clear_session() 
+tf.config.list_physical_devices('GPU')
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    tf.config.experimental.set_memory_growth(gpus[0], True)
+    tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+  except RuntimeError as e:
+    print(e)
+
+#___________________________________________________
