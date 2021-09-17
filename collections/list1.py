8943929b1df8154a63f8a1a7f761dbab1910fa68
entradajuan <entradajuan@yahoo.es>
2021-09-17 16:04:17 +0000

    collections

--- collections/list1.py
+++ collections/list1.py
@@ -13,14 +13,23 @@ list2 = ["AAA", "BBB", "CCC"]
 def strip(word):
   return word.lower()
 
-list2 = [strip(w) for w in list2]
+#list2 = [strip(w) for w in list2]
+list2 = [(lambda w : w.lower())(w) for w in list2]
+
 print(list2)
 
 first = lambda w : w[:1]
 list2 = map(first, list2)
-print(type(list2))
-for e in list2:
-  print(e)
+
+#for e in list2:
+#  print(e)
+
+print(list2)
+sol = list(list2)
+print(sol)
   
+
+
+
 
 
